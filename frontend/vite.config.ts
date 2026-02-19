113605f640aed522b1fa0a41cf1ecdbb578f4564
Davitroon <deivitroon@gmail.com>
2026-02-19 20:06:35 +0100

    build: Remplazar Node por Bun y agregar ECharts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  server: {
+    watch: {
+      usePolling: true, // Útil para que los cambios en Windows se vean en Docker
+    },
+    host: true, // Necesario para exponer el puerto
+    strictPort: true,
+  }
 })
