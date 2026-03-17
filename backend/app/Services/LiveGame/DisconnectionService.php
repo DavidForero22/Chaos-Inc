33a276f99c1a9878d44e6ecea1f786ad68fba2fc
DavidForero22 <deivitroon@gmail.com>
2026-03-17 19:53:15 +0100

    fix: Corregir constructor fantasma petando todo el servidor
    madre mia estas dos lineas me la han liao pero bien jajaja

--- backend/app/Services/LiveGame/DisconnectionService.php
+++ backend/app/Services/LiveGame/DisconnectionService.php
@@ -8,7 +8,6 @@ use App\Events\ActingBossGracePeriodCancelled;
 use App\Events\ActingBossGracePeriodStarted;
 use App\Events\RoomStateUpdated;
 use App\Jobs\InheritBossRoleJob;
-use App\Services\LiveRoomService;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 
@@ -16,7 +15,6 @@ class DisconnectionService
 {
     public function __construct(
         protected GameFinalizationService $finalizationService,
-        protected LiveRoomService $liveRoomService,
     ) {}
 
     /**
