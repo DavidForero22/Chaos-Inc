f1330fdd7843ca006e53e5648f8b3a175bdfce09
DavidForero22 <deivitroon@gmail.com>
2026-06-09 00:13:19 +0200

    wqojeqwnjewqnjbewqbkebwqkebwqebwq

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://chaosinc.monolook.dev'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
