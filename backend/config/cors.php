75cb427bc6341b5d8848416dde67c82c8b689f6a
DavidForero22 <deivitroon@gmail.com>
2026-06-09 17:44:41 +0200

    fix: intentar arreglar despliege

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -18,7 +18,8 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:5173'),
+        env('FRONTEND_URL'),
+        'https://chaosinc.monolook.dev',
         'http://localhost:5173',
         'http://127.0.0.1:5173'
     ],
